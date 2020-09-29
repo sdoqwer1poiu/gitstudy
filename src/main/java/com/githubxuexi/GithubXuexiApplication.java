191076400db3b81a056ffaf081bcae61aa99b05c
yanghao <573968217@qq.com>
2020-09-29 11:52:40 +0800

    第一次提交

---
+++ src/main/java/com/githubxuexi/GithubXuexiApplication.java
@@ -0,0 +1,13 @@
+package com.githubxuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubXuexiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubXuexiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/githubxuexi/GithubXuexiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.githubxuexi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubXuexiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
